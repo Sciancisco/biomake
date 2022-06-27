6ef42da181a3fed1c473b73ddf3357c6cb6d1ae1
Francisco Pascoa <francisco.pascoa@umontreal.ca>
2022-06-27 10:10:40 -0400

    clean useless code

--- biomake.py
+++ biomake.py
@@ -1,4 +1,3 @@
-from _xxsubinterpreters import destroy
 from typing import Annotated, Literal, TypeVar
 import numpy.typing as npt
 
@@ -15,22 +14,6 @@ Mat3x3 = Annotated[npt.NDArray[DType], Literal[3, 3]]
 
 
 O = np.zeros(3)
-
-
-def rotation_matrix_from_vectors(vec1, vec2):
-    """ Find the rotation matrix that aligns vec1 to vec2
-    source [https://stackoverflow.com/questions/45142959/calculate-rotation-matrix-to-align-two-vectors-in-3d-space]
-    :param vec1: A 3d "source" vector
-    :param vec2: A 3d "destination" vector
-    :return mat: A transform matrix (3x3) which when applied to vec1, aligns it with vec2.
-    """
-    a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
-    v = np.cross(a, b)
-    c = np.dot(a, b)
-    s = np.linalg.norm(v)
-    kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-    rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
-    return rotation_matrix
 
 
 def format_vec(vec):
